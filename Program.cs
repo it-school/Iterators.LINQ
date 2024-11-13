efcca840d02b5cafeb8d5d348ad44f3690a1c7dd
vencedor <it@osvita.od.ua>
2024-11-13 11:34:18 +0200

    Update2024

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Concurrent;
 
 namespace LINQ
 {
@@ -20,6 +21,7 @@ namespace LINQ
 
             Console.WriteLine("\n");
         }
+
 
         public class DaysOfTheWeek : IEnumerable
         {
